4e327f88788ff95b0f59b242c9578302322a6222
Thamsanqa <ronduze@gmail.com>
2023-03-16 18:25:32 -0700

    Hello World C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,7 @@
  * main - 101-quote.c
  * description - Write a C program that prints exactly and that piece of art is useful\" 
  * - Dora kopar, 2015-10-19, followed by a new line, to the stardard error.
- *
- * Return: Always (0) (Success)
+ * Return: Always (1) (Success)
  */
 int main(void)
 {
