a59949604a8d4a7912a6e9006a68c874b3bb930b
Thamsanqa <ronduze@gmail.com>
2023-07-10 16:00:12 -0700

    0x15-file_io

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * create_file - this function creates a file.
+ * @filename: This a pointer to the name of the file that is to be created.
+ * @text_content: This points to a string that is used to write to the file.
+ *
+ * Return: If the function fails it will return -1.
+ *         Otherwise it will return 1 on success.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int df, b, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	df = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	b = write(fd, text_content, len);
+
+	if (df == -1 || b == -1)
+		return (-1);
+
+	close(df);
+
+	return (1);
+}
